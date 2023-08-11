7ce813cde7c15737433d374620cf139e88189081
Murilo Oliveira <81969402+muaoliveira@users.noreply.github.com>
2023-08-11 17:51:10 -0300

    Add files via upload

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,12 +1,36 @@
-
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
 
-  List<Map<String, Object>> get groupedTransactions{
-    return 
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+      for(int i =0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYear = recentTransaction[i].date.year == weekDay.year;
+
+        if(sameDay && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    });
   }
 
   @override
@@ -15,11 +39,8 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [
-
-        ],
+        children: [],
       ),
     );
-
   }
 }
