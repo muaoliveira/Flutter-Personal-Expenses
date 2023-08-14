e7e23ee570e21d9675ed9a7e6353a26e84136be4
Murilo Oliveira <81969402+muaoliveira@users.noreply.github.com>
2023-08-14 16:37:59 -0300

    Add files via upload

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -14,81 +14,62 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: transaction.isEmpty ? Column(
-        children: [
-          SizedBox(
-            height: 25,
-          ),
-          Text(
-            'Nenhuma despesa cadastrada',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          SizedBox(
-            height: 35,
-          ),
-          Container(
-            height: 200,
-            child: Image.asset('assets/images/waiting.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-        ],
-      ) 
-      : ListView.builder(
-        itemCount: transaction.length,
-        itemBuilder: (ctx, index) {
-          final tr = transaction[index];
-          return Card(
-              child: Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
+      child: transaction.isEmpty
+          ? Column(
+              children: [
+                SizedBox(
+                  height: 25,
                 ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Theme.of(context).colorScheme.primary,
-                  width: 2,
-                )),
-                padding: const EdgeInsets.all(15),
-                width: 125,
-                child: Center(
-                  child: FittedBox(
-                    child: Text(
-                      'R\$${tr.value.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 20,
+                Text(
+                  'Nenhuma despesa cadastrada',
+                  style: Theme.of(context).textTheme.titleLarge,
+                  
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transaction.length,
+              itemBuilder: (ctx, index) {
+                final tr = transaction[index];
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Colors.white,
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text('R\$${tr.value}'),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    tr.title,
-                      style: const TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                      fontFamily: 'OpenSans',
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
                     ),
                   ),
-                  Text(
-                    DateFormat('d MMM y').format(tr.date),
-                    style: const TextStyle(
-                      color: Color.fromARGB(255, 65, 65, 65),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ));
-        },
-      ),
+                );
+              },
+            ),
     );
   }
 }
